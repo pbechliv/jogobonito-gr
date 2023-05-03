c0b717eb06dab2a5364d6402b8c22e995d82cf2b
Panos Bechlivanos <panos.bechlivanos@remberg.de>
2023-05-03 19:46:08 +0200

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@jogo/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin", "greek"] });
 
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`pb-10 ${inter.className}`}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
