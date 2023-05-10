f9e25aeca682bf6e6ba67b17182d84b8848c52ef
Panos Bechlivanos <panos.bechlivanos@remberg.de>
2023-05-10 15:25:37 +0200

    add date, author | remove redundant images from public

--- src/app/post/[slug]/post-page.tsx
+++ src/app/post/[slug]/post-page.tsx
@@ -1,6 +1,7 @@
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import {
   BLOCKS,
+  INLINES,
   Block,
   Inline,
   MARKS,
@@ -9,6 +10,8 @@ import {
 import Layout from "@jogo/components/layout";
 import RichTextAsset from "@jogo/components/rich-text-asset";
 import { PostContent, PostWithContent, Tag } from "@jogo/definitions";
+import { HOSTNAME } from "@jogo/definitions/hostname";
+import { format } from "date-fns";
 import NextImage from "next/image";
 import { Fragment } from "react";
 
@@ -35,6 +38,17 @@ const customMarkdownOptions = (content: PostContent) => ({
         assets={content.links.assets.block}
       />
     ),
+    [INLINES.HYPERLINK]: (node: Inline | Block, children: React.ReactNode) => {
+      console.log(node.data.uri);
+      return (
+        <a
+          href={node.data.uri}
+          target={node.data.uri.startsWith(HOSTNAME) ? "_self" : "_blank"}
+        >
+          {children}
+        </a>
+      );
+    },
   },
   renderMark: {
     [MARKS.CODE]: (node: React.ReactNode) => {
@@ -65,8 +79,20 @@ const PostPage = ({ post, tags }: PostPageProps) => {
   return (
     <Layout tags={tags}>
       <div className="px-4">
-        <div className="prose max-w-full mb-3">
+        <div className="prose max-w-full mb-3 text-center">
           <h1>{post.title}</h1>
+        </div>
+        <div className="flex gap-3 justify-center items-center mb-3">
+          <span>{format(new Date(post.publishedDate), "dd/MM/yyyy")}</span>
+          <span> | </span>
+          <span>Νεκτάριος Δαργάκης</span>
+          <NextImage
+            className="rounded-full"
+            src="/nectarios.jpeg"
+            width={36}
+            height={36}
+            alt=""
+          />
         </div>
         <div className="relative aspect-video mb-3">
           <NextImage
