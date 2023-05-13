0ae6a24c32611730e8572b1b8ea1c0ea4d339dfd
Panos Bechlivanos <panos.bechlivanos@remberg.de>
2023-05-13 17:56:08 +0200

    adapt category dialog

--- src/components/categories.tsx
+++ src/components/categories.tsx
@@ -7,8 +7,8 @@ interface CategoriesProps {
 
 export const Categories = ({ tags }: CategoriesProps) => {
   return (
-    <div className="text-center h-full p-4">
-      <ul className="h-full overflow-y-scroll">
+    <div className="text-center">
+      <ul>
         {tags.map((tag) => (
           <li key={tag.slug} className="p-2 border-b-2 border-yellow-200">
             <Link href={`/tag/${tag.slug}/1`}>{tag.name}</Link>
--- src/components/nav-dialog.tsx
+++ src/components/nav-dialog.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import { Dialog, Tab, Transition } from "@headlessui/react";
-import { Fragment, useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Tag } from "@jogo/definitions";
+import { Fragment, useState } from "react";
 import { Categories } from "./categories";
 
 interface NavDialogProps {
@@ -12,7 +14,7 @@ interface NavDialogProps {
 export default function NavDialog({ tags, className }: NavDialogProps) {
   let [isOpen, setIsOpen] = useState(false);
 
-  const tabs = ["Βασικές", "Όλες"];
+  const tabs = ["Βασικές", "Άλλες"];
   const mainTagNamesSorted = [
     "Αφιερώματα",
     "Ποδόσφαιρο",
@@ -41,15 +43,13 @@ export default function NavDialog({ tags, className }: NavDialogProps) {
         onClick={openModal}
         className={`
             h-10 w-10 
-            rounded-full
-          hover:bg-yellow-200
-          active:bg-yellow-300`}
+            rounded-full`}
       >
         <Bars3Icon className="h-6 w-6 m-auto" />
       </button>
 
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Dialog onClose={closeModal}>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -62,64 +62,53 @@ export default function NavDialog({ tags, className }: NavDialogProps) {
             <div className="fixed inset-0 bg-black bg-opacity-25" />
           </Transition.Child>
 
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex h-screen min-w-full items-center justify-center text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="max-md:h-full max-md:w-full h-96 w-96 transform overflow-hidden bg-white p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title
-                    as="div"
-                    className="flex justify-between items-center"
+          <div className="fixed inset-0 flex justify-end items-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="bg-white h-full w-3/4 p-6 flex flex-col">
+                <div className="flex justify-between items-center">
+                  <Dialog.Title className="text-2xl">Κατηγορίες</Dialog.Title>
+                  <button
+                    onClick={closeModal}
+                    className={`h-10 w-10 rounded-full`}
                   >
-                    <h3 className=" text-2xl">Κατηγορίες</h3>
-                    <button
-                      onClick={closeModal}
-                      className={`
-                        h-10 w-10 
-                        rounded-full
-                      hover:bg-yellow-200
-                      active:bg-yellow-300`}
-                    >
-                      <XMarkIcon className="h-6 w-6 m-auto" />
-                    </button>
-                  </Dialog.Title>
-                  <Tab.Group>
-                    <Tab.List className={"mt-2 flex gap-4 justify-evenly"}>
-                      {tabs.map((tab) => (
-                        <Tab key={tab} as={Fragment}>
-                          {({ selected }) => (
-                            /* Use the `selected` state to conditionally style the selected tab. */
-                            <button
-                              className={`
+                    <XMarkIcon className="h-6 w-6 m-auto" />
+                  </button>
+                </div>
+                <Tab.Group>
+                  <Tab.List className={"mt-2 flex gap-4 justify-evenly"}>
+                    {tabs.map((tab) => (
+                      <Tab key={tab} as={Fragment}>
+                        {({ selected }) => (
+                          <button
+                            className={`
                                 rounded-full text-lg px-2 py-1 w-36
                               ${selected ? "bg-yellow-200" : "bg-white"}`}
-                            >
-                              {tab}
-                            </button>
-                          )}
-                        </Tab>
-                        // <Tab key={tab}>{tab}</Tab>
-                      ))}
-                    </Tab.List>
-                    <Tab.Panels className={"h-full"}>
-                      <Tab.Panel className={"h-full"}>
-                        <Categories tags={mainTags} />
-                      </Tab.Panel>
-                      <Tab.Panel className={"h-full"}>
-                        <Categories tags={secondaryTags} />
-                      </Tab.Panel>
-                    </Tab.Panels>
-                  </Tab.Group>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
+                          >
+                            {tab}
+                          </button>
+                        )}
+                      </Tab>
+                    ))}
+                  </Tab.List>
+                  <Tab.Panels className="mt-2 overflow-y-scroll">
+                    <Tab.Panel>
+                      <Categories tags={mainTags} />
+                    </Tab.Panel>
+                    <Tab.Panel>
+                      <Categories tags={secondaryTags} />
+                    </Tab.Panel>
+                  </Tab.Panels>
+                </Tab.Group>
+              </Dialog.Panel>
+            </Transition.Child>
           </div>
         </Dialog>
       </Transition>
