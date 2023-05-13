8025c42ad58731cd9c3330b6b98ab2ce3d27c05a
Panos Bechlivanos <panos.bechlivanos@remberg.de>
2023-05-14 00:19:29 +0200

    remove analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "@jogo/styles/globals.css";
-import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin", "greek"] });
 
@@ -12,7 +12,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <Analytics />
+      {process.env.NODE_ENV === "production" && <Analytics />}
     </html>
   );
 }
